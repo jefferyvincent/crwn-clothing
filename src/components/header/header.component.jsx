b96ecc136e7fcbfd85729c701932cbf3cdd80540
Jeffery Vincent <jeffery.vicnent@msn.com>
2021-02-05 16:34:47 -0500

    refactoring components to use reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,9 @@ import './header.styles.scss';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo } from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
+import { createStructuredSelector } from 'reselect';
+import { selectCartHidden} from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from "../../redux/user/user.selector";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {connect} from 'react-redux';
@@ -33,9 +36,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart:{hidden}}) => ({
-    currentUser,
-    hidden
-})
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+});
 
 export default connect(mapStateToProps)(Header);
