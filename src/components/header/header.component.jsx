973fb771c85f469b18f6956c892decd6bfa464a2
Jeffery Vincent <jeffery.vicnent@msn.com>
2021-02-11 12:37:53 -0500

    converted the custom button to a styled component using props

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
-import {Link} from 'react-router-dom';
-import {ReactComponent as Logo } from '../../assets/crown.svg';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import { createStructuredSelector } from 'reselect';
 import { selectCartHidden} from '../../redux/cart/cart.selectors';
@@ -8,7 +8,7 @@ import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {connect} from 'react-redux';
 
-import {HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink} from './header.styles';
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
     <HeaderContainer>
@@ -24,7 +24,7 @@ const Header = ({ currentUser, hidden }) => (
             </OptionLink>
             {
                 currentUser ?
-                <OptionDiv onClick={() => auth.signOut()}> SIGN OUT</OptionDiv>
+                <OptionLink as="div" onClick={() => auth.signOut()}> SIGN OUT</OptionLink>
                 :
                 <OptionLink to="/signin">SIGN IN</OptionLink>
             }
